19533e3409ac00b9fd99cb2c2c872ed4c0561731
Samuel Lorch <sam@lorch.net>
2021-09-02 09:26:47 +0200

    add common utility functions

---
+++ helper/util.go
@@ -0,0 +1,34 @@
+package helper
+
+import (
+	"fmt"
+
+	"github.com/speatzle/go-passbolt/api"
+)
+
+func getPublicKeyByUserID(userID string, Users []api.User) (string, error) {
+	for _, user := range Users {
+		if user.ID == userID {
+			return user.GPGKey.ArmoredKey, nil
+		}
+	}
+	return "", fmt.Errorf("Cannot Find Key for user id %v", userID)
+}
+
+func getMembershipByUserID(memberships []api.GroupMembership, userID string) (*api.GroupMembership, error) {
+	for _, membership := range memberships {
+		if membership.UserID == userID {
+			return &membership, nil
+		}
+	}
+	return nil, fmt.Errorf("Cannot Find Membership for user id %v", userID)
+}
+
+func getSecretByResourceID(secrets []api.Secret, resourceID string) (*api.Secret, error) {
+	for _, secret := range secrets {
+		if secret.ResourceID == resourceID {
+			return &secret, nil
+		}
+	}
+	return nil, fmt.Errorf("Cannot Find Secret for id %v", resourceID)
+}
